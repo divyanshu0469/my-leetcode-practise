49cf72fbbcca446b2bdd9bf3840c0e0c91c23e27
Divyanshu Yadav <102656731+divyanshu0469@users.noreply.github.com>
2023-09-05 21:02:19 +0530

    (040) 561. Array Partition.java

---
+++ (040) 561. Array Partition.java
@@ -0,0 +1,11 @@
+/* URL : https://leetcode.com/problems/array-partition/ */
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max = 0;
+        for (int i = 0; i < nums.length; i+=2) {
+            max += nums[i];
+        }
+        return max;
+    }
+}
