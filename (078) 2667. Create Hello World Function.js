c7ca9318c577972379189d43c298c8d340016e61
Divyanshu Yadav <102656731+divyanshu0469@users.noreply.github.com>
2024-02-26 22:41:46 +0530

    (078) 2667. Create Hello World Function.js

---
+++ (078) 2667. Create Hello World Function.js
@@ -0,0 +1,12 @@
+/* URL : https://leetcode.com/problems/create-hello-world-function/ */
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {return "Hello World"};
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
