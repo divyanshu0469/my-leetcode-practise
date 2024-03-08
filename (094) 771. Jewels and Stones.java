0bb6393d76bb808535abb7c0b269b6d4787a31f2
Divyanshu Yadav <102656731+divyanshu0469@users.noreply.github.com>
2024-03-08 23:30:12 +0530

    (094) 771. Jewels and Stones.java

---
+++ (094) 771. Jewels and Stones.java
@@ -0,0 +1,12 @@
+/* URL : https://leetcode.com/problems/jewels-and-stones/description/ */
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (char j: jewels.toCharArray()) {
+            for (char s: stones.toCharArray()) {
+                if (j == s) count++;
+            }
+        }
+        return count;
+    }
+}
