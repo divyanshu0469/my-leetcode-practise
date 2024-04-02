815add1f8406766abe560ebf6cb808238d1464f5
Divyanshu Yadav <102656731+divyanshu0469@users.noreply.github.com>
2024-04-02 22:43:14 +0530

    (123) 921. Minimum Add to Make Parentheses Valid.java

---
+++ (123) 921. Minimum Add to Make Parentheses Valid.java
@@ -0,0 +1,19 @@
+/* URL : https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/ */
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int left = 0;
+        int right = 0;
+        for(char c: s.toCharArray()) {
+            if(c == '(') {
+                left++;
+            } else {
+                if(left > 0) {
+                    left--;
+                } else {
+                    right++;
+                }
+            }
+        }
+        return left+right;
+    }
+}
